4c7422820e4c352e4887b743920287958741d290
Neffrey <neffreyl@gmail.com>
2024-09-20 01:58:44 -0500

    Chore: UploadThing configs

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,5 @@
 // LIBS
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
 
 // UTILS
 import { getServerAuthSession } from "~/server/auth";
@@ -12,16 +11,19 @@ export const ourFileRouter = {
   // Define as many FileRoutes as you like, each with a unique routeSlug
   imageUploader: f({ image: { maxFileSize: "4MB" } })
     // Set permissions and file types for this FileRoute
-    .middleware(async (/*{ req }*/) => {
+    .middleware(async () => {
       // This code runs on your server before upload
       const user = await getServerAuthSession();
 
       // If you throw, the user will not be able to upload
-      if (!user)
-        throw new UploadThingError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Must be logged in to upload",
-        });
+      if (!user) {
+        throw new Error("Must be logged in to upload");
+      }
+      // if (!user?.user) console.log("no user");
+      // throw new UploadThingError({
+      //   code: "INTERNAL_SERVER_ERROR",
+      //   message: "Must be logged in to upload",
+      // });
 
       // Whatever is returned here is accessible in onUploadComplete as `metadata`
       return { userId: user.user.id };
