4e7369636560a2d47a799ef3292652b5bcb30fa9
Neffrey <neffreyl@gmail.com>
2024-10-17 17:57:00 -0500

    Feat: Product table column work

--- src/app/edit-products/_components/table/columns.tsx
+++ src/app/edit-products/_components/table/columns.tsx
@@ -11,14 +11,29 @@ import { type Product } from "~/server/db/schema";
 export const columns: ColumnDef<Product>[] = [
   {
     accessorKey: "title",
-    header: "Title",
+    header: () => {
+      return <TableHeader>Title</TableHeader>;
+    },
   },
   {
     accessorKey: "tags",
-    header: "Tags",
+    header: () => {
+      return <TableHeader>Tags</TableHeader>;
+    },
   },
   {
     accessorKey: "description",
-    header: "Description",
+    header: () => {
+      return <TableHeader>Description</TableHeader>;
+    },
+    // cell: ({ row }) => {
+    //   return (
+    //     <CreateCompletionBtn className="justify-end px-4" task={row.original} />
+    //   );
+    // },
   },
 ];
+
+export const TableHeader = ({ children }: { children: React.ReactNode }) => {
+  return <div className="flex justify-end px-2">{children}</div>;
+};
